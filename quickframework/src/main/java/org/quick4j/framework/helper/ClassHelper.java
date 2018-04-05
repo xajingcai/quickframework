7ff607a5a402c8c5cc926988f1b99cbacac550c9
xajingcai <xajingcai@163.com>
2018-04-05 23:01:53 +0800

    just test github update

--- quickframework/src/main/java/org/quick4j/framework/helper/ClassHelper.java
+++ quickframework/src/main/java/org/quick4j/framework/helper/ClassHelper.java
@@ -1,7 +1,10 @@
 package org.quick4j.framework.helper;
 
+import javafx.concurrent.Service;
 import org.quick4j.framework.util.ClassUtil;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
 
 public final class ClassHelper {
@@ -19,7 +22,26 @@ public final class ClassHelper {
     /**
      * 获取应用包名下的所有类
      */
-    public static Set<Class<?>> getClassSet(){
+    public static Set<Class<?>> getClassSet() {
         return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包下的所有service类
+     */
+    public static Set<Class<?>> getServiceClassSet()
+    {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for(Class<?> cls : CLASS_SET){
+            if(cls.isAnnotationPresent((Class<? extends Annotation>) Service.class)){
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getBeanClassSet(){
+        //TODO
+        return null;
     }
 }
